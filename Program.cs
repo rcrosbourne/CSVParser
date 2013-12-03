75f784051d16b9395867410a7b81073967fdf1a9
rcrosbourne <rcrosbourne@gmail.com>
2013-12-02 23:02:58 -0500

    CSVParser works

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ComLib.CsvParse;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace CSVParser
 {
@@ -10,6 +12,17 @@ namespace CSVParser
     {
         static void Main(string[] args)
         {
+            string csvString = "a,b,c,\"d,c,e\",f";
+            Console.Write(csvString);
+            CsvDoc csvDoc = Csv.LoadText(csvString, false);
+
+            List<string> records = new List<string>();
+            for (int i = 0; i < csvDoc.Columns.Count; i++)
+            {
+                records.Add(csvDoc.Data[0][i] as string) ;
+            }
+            Console.ReadKey();
+
         }
     }
 }
